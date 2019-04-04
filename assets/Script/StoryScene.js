cc5de72f2a32559897d5dee127e81a965dfdaf9b
nlh1996 <1217286494@qq.com>
2019-04-04 15:51:03 +0800

    111

--- assets/Script/StoryScene.js
+++ assets/Script/StoryScene.js
@@ -13,10 +13,9 @@ cc.Class({
 
     start () {
       this.labelSchedule(story[0].about)
-      this.btn.node.on('click',()=>{
-        GameSceneMng.getInstance().setGameScene(GAME_SCENE.GAME)
-      })
+      this.btn.node.on('click', this.startGame, this)
     },
+
 
     // 文字提示
     labelSchedule(content) {
@@ -28,9 +27,17 @@ cc.Class({
         this.content.string = this.content.string + content[index]
         index++
         if(index>i) {
-          GameSceneMng.getInstance().setGameScene(GAME_SCENE.GAME)
+          this.startGame()
         }
       },0.06,i,0)
     },
+
+    startGame() {
+      var finished = cc.callFunc(function () {
+        GameSceneMng.getInstance().setGameScene(GAME_SCENE.GAME)
+      })
+      var myAction = cc.sequence(cc.fadeOut(2.0), finished);
+      this.node.runAction(myAction)
+    }
     // update (dt) {},
 });
