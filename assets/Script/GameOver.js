d51d42aa72eda2477a78d91198511e21c1e9f62f
nlh1996 <1217286494@qq.com>
2019-04-18 14:11:47 +0800

    4_18

---
+++ assets/Script/GameOver.js
@@ -0,0 +1,26 @@
+// 游戏结束脚本
+cc.Class({
+    extends: cc.Component,
+    properties: {
+      btn1: cc.Button,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+      //获取 ArmatureDisplay
+      this._armatureDisPlay = this.getComponent(dragonBones.ArmatureDisplay)
+      this.btn1.node.on('click',this.callback,this)
+        //获取 Armatrue
+        // this._armature = this._armatureDisPlay.armature()
+        // this._armature.animation.fadeIn('attack1', -1, -1, 0, 'hit');
+        
+    },
+
+    callback () {
+      this._armatureDisPlay.playAnimation('attack1', 1);
+    },
+
+
+    // update (dt) {},
+});
